8d91e69c1a2cc5f8ba46e619621e246aa6e8798c
JoseLuisReyesPerez <124086822+JoseLuisReyesPerez@users.noreply.github.com>
2023-02-15 22:08:49 -0600

    a (#6)
    * Update Main.java
    
    * Update Main.java
    
    * Agregado comentarioo jaja
    
    Agregado comentarioo jaja
    
    * Update Main.java
    
    * Update Main.java
    
    * Update Main.java (#4)
    
    * Nuevo comentario
    
    * Nuevo comentario
    
    * Nuevo archivo agregado desde el fork (#5)
    
    Mergeado
    
    ---------
    
    Co-authored-by: ReyinLight <52404859+ReyinLight@users.noreply.github.com>
    Co-authored-by: Reyin <ReyinLight@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Nuevo cambio");
+        System.out.println("Rama principal editada");
+        // nuevo
+        
+        for(int i = 0; i<10; i++){
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
+
+        }
+
+        
     }
 }
